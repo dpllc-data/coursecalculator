52ce904a8f237514b845ea47990cc33dcf6d05bb
dpllc-data <stanshelton@gmail.com>
2022-04-23 23:40:15 -0400

    Commit Update 04232022: Working version offline see README.md

--- src/main/java/com/eliteteachlearn/CalculatorServlet.java
+++ src/main/java/com/eliteteachlearn/CalculatorServlet.java
@@ -1,5 +1,20 @@
+/**
+* The purpose of this servlet class is to service HTTP requests from the
+* web client class. More specifically the class handles the backend tasks
+* associated with; obtaining user input for the last assignment completed,
+* loading time data needed to produce user results, creating a subset of
+* that data that is pertinent to the user, storing the resulting data and
+* passing that data on as an HTTP service request.
+ *
+ * author: Stan Shelton
+ * version: 1.05
+ * date: April 23, 2022
+ */
+
+// IntelliJ create package
 package com.eliteteachlearn;
 
+// import statements
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -8,47 +23,72 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+// Servlet class handles user input, aggregation of data, and posting of desired data
 @WebServlet("/calculate")
 public class CalculatorServlet extends HttpServlet {
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws IOException {
-        String input = request.getParameter("lastSect");
+    @Override // Override service method allowing servlet to handle requests
+    /**
+     * The service method is handling the footwork (input, aggregation, storage and posting)
+     * @param request creates object passed as argument
+     * @param response creates object passed as argument
+     * @throws IOException if an input or output error occurs while the servlet is handling the HTTP request
+     * @throws ServletException if the HTTP request cannot be handled
+     */
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        //input for last assignment completed and the corresponding course
+        String lastSect = request.getParameter("lastSect");
+        String courseName = request.getParameter("courseName");
 
-        File fileName = new File(getServletContext().getRealPath("assessment_times.txt"));
+        // control statement for loading the corresponding course data
+        File fileName = null;
+        if (courseName.equals("APCSA1"))
+            fileName = new File(getServletContext().getRealPath("APCSA1.txt"));
+        if (courseName.equals("APCSA2"))
+            fileName = new File(getServletContext().getRealPath("APCSA2.txt"));
+        if (courseName.equals("DIT1"))
+            fileName = new File(getServletContext().getRealPath("DITSem1.txt"));
+        if (courseName.equals("DIT2"))
+            fileName = new File(getServletContext().getRealPath("DITSem2.txt"));
+        if (courseName.equals("FOP1"))
+            fileName = new File(getServletContext().getRealPath("FOPSem1.txt"));
+        if (courseName.equals("FOP2"))
+            fileName = new File(getServletContext().getRealPath("FOPSem2.txt"));
+
+        // scanner in find the length of the text file holding the corresponding data
         Scanner inFile = new Scanner(fileName);
 
         int totAssessments = 0;
 
-        while (inFile.hasNextLine())
-        {
+        while (inFile.hasNextLine()) {
             inFile.nextLine();
             totAssessments++;
         }
         inFile.close();
 
-        String [] assessName = new String [totAssessments];
-        int [] sectMin = new int [totAssessments];
-        int [] totMin = new int [totAssessments];
-        double [] totHoursDeci = new double [totAssessments];
-        int [] totHrsTrunc = new int [totAssessments];
-        double [] totMinLftOvr = new double [totAssessments];
-        double [] totHours = new double [totAssessments];
-        int [] partHrsToDay = new int [totAssessments];
-        int [] noDays1Hr = new int [totAssessments];
-        double [] noWeeksDaily = new double [totAssessments];
-        double [] noWeeksWkdys = new double [totAssessments];
-        int [] noDays2Hr = new int [totAssessments];
-        double [] noWks2HrsDaily = new double [totAssessments];
-        double [] noWks2HrsWkdys = new double [totAssessments];
-        int [] noDays1HrDayWkDays = new int [totAssessments];
-        int [] noDays2HrDayWkDays = new int [totAssessments];
+        // Data structure declaration and initialization
+        String[] assessName = new String[totAssessments];
+        int[] sectMin = new int[totAssessments];
+        int[] totMin = new int[totAssessments];
+        double[] totHoursDeci = new double[totAssessments];
+        int[] totHrsTrunc = new int[totAssessments];
+        double[] totMinLftOvr = new double[totAssessments];
+        double[] totHours = new double[totAssessments];
+        int[] partHrsToDay = new int[totAssessments];
+        int[] noDays1Hr = new int[totAssessments];
+        double[] noWeeksDaily = new double[totAssessments];
+        double[] noWeeksWkdys = new double[totAssessments];
+        int[] noDays2Hr = new int[totAssessments];
+        double[] noWks2HrsDaily = new double[totAssessments];
+        double[] noWks2HrsWkdys = new double[totAssessments];
+        int[] noDays1HrDayWkDays = new int[totAssessments];
+        int[] noDays2HrDayWkDays = new int[totAssessments];
 
+        // read corresponding course data from text file into data structures
         inFile = new Scanner(fileName);               //reopen the file
         int index = 0;
 
-        while (inFile.hasNext())
-        {
+        while (inFile.hasNext()) {
             assessName[index] = inFile.next();
             sectMin[index] = inFile.nextInt();
             totMin[index] = inFile.nextInt();
@@ -69,45 +109,100 @@ public class CalculatorServlet extends HttpServlet {
         }
         inFile.close();
 
+        // Create and load up separate data structure containing only pertinent results
         ArrayList<String> results = new ArrayList<String>();
-        for(int n = 0; n < totAssessments; n++)
-        {
+        for (int n = 0; n < totAssessments; n++){
             //System.out.println(assessName[n].substring(0,5));
-            if (input.equals(assessName[n].substring(0,5)))
-            {
-                results.add( new String("You last completed the " + assessName[n] + " assessment."));
-                results.add( new String("You next assessment is the " + assessName[n+1] + " assessment."));
+            if (lastSect.equals(assessName[n].substring(0, 5))) {
 
-                results.add( new String("Your time left in the course is approximately " + totMin[n] + " minutes."));
-                results.add( new String("Your time left in the course is approximately " + totHours[n] + " hours."));
+                // build BOTH request object && ArrayList structure for flexibility
+                results.add(new String("Your last completed the " + assessName[n] + " assessment."));
+                request.setAttribute("line1", "Your last completed the " + assessName[n] + " assessment.");
+                results.add(new String("Your next assessment is the " + assessName[n + 1] + " assessment."));
+                request.setAttribute("line2", "Your next assessment is the " + assessName[n + 1] + " assessment.");
 
-                results.add( new String("If you are willing to put in an 1hr/day, 5 days/wk!"));
-                results.add( new String("Your time left in the course is approximately " + noDays1HrDayWkDays[n] + " days working 1hr/day - 5days/wk."));
-                results.add( new String("Your time left in the course is approximately " + noWeeksWkdys[n] + " weeks working 1hr/day - 5days/wk."));
+                results.add(new String("TOTAL TIME: Your time left in the course is approximately " + totMin[n] + " minutes."));
+                request.setAttribute("line3", "TOTAL TIME: Your time left in the course is approximately " + totMin[n] + " minutes.");
+                results.add(new String("TOTAL TIME: Your time left in the course is approximately " + totHours[n] + " hours."));
+                request.setAttribute("line4", "TOTAL TIME: Your time left in the course is approximately " + totHours[n] + " hours.");
 
-                results.add( new String("If you are willing to put in an 2hr/day, 5 days/wk!"));
-                results.add( new String("Your time left in the course is approximately " + noDays2HrDayWkDays[n] + " days working 2hrs/day - 5days/wk."));
-                results.add( new String("Your time left in the course is approximately " + noWks2HrsWkdys[n] + " weeks working 1hr/day - 5days/wk."));
+                results.add(new String("If you are willing to put in an 1hr/day, 5 days/wk!"));
+                request.setAttribute("line5", "If you are willing to put in an 1hr/day, 5 days/wk!");
+                results.add(new String("Your time left in the course is approximately " + noDays1HrDayWkDays[n] + " days working 1hr/day - 5days/wk."));
+                request.setAttribute("line6", "Your time left in the course is approximately " + noDays1HrDayWkDays[n] + " days working 1hr/day - 5days/wk.");
+                results.add(new String("Your time left in the course is approximately " + noWeeksWkdys[n] + " weeks working 1hr/day - 5days/wk."));
+                request.setAttribute("line7", "Your time left in the course is approximately " + noWeeksWkdys[n] + " weeks working 1hr/day - 5days/wk.");
 
-                results.add( new String("If you are willing to put in an 1hr/day, 7 days/wk!"));
-                results.add( new String("Your time left in the course is approximately " + noDays1Hr[n] + " days working 1hr/day - 7days/wk."));
-                results.add( new String("Your time left in the course is approximately " + noWeeksDaily[n] + " weeks working 1hr/day - 7days/wk."));
+                results.add(new String("If you are willing to put in an 2hrs/day, 5 days/wk!"));
+                request.setAttribute("line8", "If you are willing to put in an 2hrs/day, 5 days/wk!");
+                results.add(new String("Your time left in the course is approximately " + noDays2HrDayWkDays[n] + " days working 2hrs/day - 5days/wk."));
+                request.setAttribute("line9", "Your time left in the course is approximately " + noDays2HrDayWkDays[n] + " days working 2hrs/day - 5days/wk.");
+                results.add(new String("Your time left in the course is approximately " + noWks2HrsWkdys[n] + " weeks working 1hr/day - 5days/wk."));
+                request.setAttribute("line10", "Your time left in the course is approximately " + noWks2HrsWkdys[n] + " weeks working 2hr/day - 5days/wk.");
 
-                results.add( new String("If you are willing to put in an 2hrs/day, 7 days/wk!"));
-                results.add( new String("Your time left in the course is approximately " + noDays2Hr[n] + " days working 2hrs/day - 7days/wk."));
-                results.add( new String("Your time left in the course is approximately " + noWks2HrsDaily[n] + " weeks working 2hrs/day - 7days/wk."));
+                results.add(new String("If you are willing to put in an 1hr/day, 7 days/wk!"));
+                request.setAttribute("line11", "If you are willing to put in an 1hr/day, 7 days/wk!");
+                results.add(new String("Your time left in the course is approximately " + noDays1Hr[n] + " days working 1hr/day - 7days/wk."));
+                request.setAttribute("line12", "Your time left in the course is approximately " + noDays1Hr[n] + " days working 1hr/day - 7days/wk.");
+                results.add(new String("Your time left in the course is approximately " + noWeeksDaily[n] + " weeks working 1hr/day - 7days/wk."));
+                request.setAttribute("line13", "Your time left in the course is approximately " + noWeeksDaily[n] + " weeks working 1hr/day - 7days/wk.");
+
+                results.add(new String("If you are willing to put in an 2hrs/day, 7 days/wk!"));
+                request.setAttribute("line14", "If you are willing to put in an 2hrs/day, 7 days/wk!");
+                results.add(new String("Your time left in the course is approximately " + noDays2Hr[n] + " days working 2hrs/day - 7days/wk."));
+                request.setAttribute("line15", "Your time left in the course is approximately " + noDays2Hr[n] + " days working 2hrs/day - 7days/wk.");
+                results.add(new String("Your time left in the course is approximately " + noWks2HrsDaily[n] + " weeks working 2hrs/day - 7days/wk."));
+                request.setAttribute("line16", "Your time left in the course is approximately " + noWks2HrsDaily[n] + " weeks working 2hrs/day - 7days/wk.");
+                request.setAttribute("line17", "-------------------------------------------------------------");
+
+            }
+    }
+            // catches improper requests made by user; forces restart of user prompt
+            if (results.isEmpty()) {
+                results.add(new String("You entered " + lastSect + ". Please go back. Check that you are have selected the correct course " +
+                        " AND the correct semester!"));
+                request.setAttribute("line1", "You entered " + lastSect + ". Please go back. Check that you are have selected the correct course " +
+                        " AND the correct semester.");
             }
 
-        }
+/*
+            //String test = results.get(10);
+            response.getWriter().println("APCSA2 Time Left in Course Estimator");
+            response.getWriter().println();
+            response.getWriter().println("We all work at different speed, so use this as a relative tool, not an absolute " +
+                    "statement of time left in the course.");
+            response.getWriter().println("Make adjustments to these numbers if you see yourself working " +
+                    "faster or slower than what is estimated.");
+            response.getWriter().println();
+ */
+/*
+            for (int i = 0; i < results.size(); i++) {
+                response.getWriter().println(results.get(i));
+                if (i == 1) {
+                    response.getWriter().println();
+                }
+                if (i == 3) {
+                    response.getWriter().println();
+                }
+                if (i == 6) {
+                    response.getWriter().println();
+                }
+                if (i == 9) {
+                    response.getWriter().println();
+                }
+                if (i == 12) {
+                    response.getWriter().println();
+                }
+                if (i == 15) {
+                    response.getWriter().println();
+                }
+            }
+*/
 
-        //String test = results.get(10);
-        response.getWriter().println("Your Approx Time Left in the Course");
-        response.getWriter().println();
-        //response.getWriter().println("TSum is " + index + totAssessments);
-       // response.getWriter().println(test);
+        request.setAttribute("results", results);
+            getServletContext().getRequestDispatcher("/report").forward(request, response);
+            //response.sendRedirect("http://localhost:8080/HelloWorld/test");
 
-        for(int i=0;i<results.size();i++) {
-            response.getWriter().println(results.get(i));
         }
     }
-}
+
--- target/CourseTime-1.0-SNAPSHOT/index.jsp
+++ target/CourseTime-1.0-SNAPSHOT/index.jsp
@@ -3,27 +3,48 @@
 <html lang="en">
 <head>
     <link rel="stylesheet" href="style.css">
-    <title>APCSA Estimated Time Left Calculator</title>
+    <title>Course Time Left Calculator</title>
 </head>
 
 <body>
-<h1>APCSA Estimated Time Left Calculator</h1>
+<h1>Course Time Left Calculator</h1>
 <br>
 <p style="Font-family:'Arial',sans-serif; Font-size:18px; color:black;">
-    IMPORTANT: Please take the time to consider that all students tend to work at different paces.
+    IMPORTANT: Please take the time to consider that all students tend to work at different speeds.
     <br><br>
     Note: This calculator only serves to give you a basis upon which to estimate your time left
     in the course. If you find that you are consistently taking more time than what is
     estimated, then make adjustments to these estimates.</p>
 <p></p>
-<p style="Font-family:'Arial Black',sans-serif; Font-size:18px; color:black;">
-    DIRECTIONS: Please enter the last section that you completed (Ex: 02.10)
+
+    <br>
+
+    <form action="${pageContext.request.contextPath}/calculate" method="post">
+        <p style="Font-family:'Arial Black',sans-serif; Font-size:18px; color:black;">
+    Step 1: Select your course
+            <br>
+    <select name = "courseName">
+        <option>APCSA1</option>
+        <option>APCSA2</option>
+        <option>DIT1</option>
+        <option>DIT2</option>
+        <option>FOP1</option>
+        <option>FOP2</option>
+
+    </select>
+<br>
+        <br>
+    <p style="Font-family:'Arial Black',sans-serif; Font-size:18px; color:black;">
+    Step 2: Please enter the last section that you completed (Ex: 02.10)
     <br>
     New students should enter: 00.00</p>
-<form action="${pageContext.request.contextPath}/calculate" method="post">
-    <input type="text" name="lastSect" placeholder="Enter Last Section Submitted">
-    <input type="submit" value="Calculate Time">
+    <input type="text" name="lastSect" placeholder="Enter Last Section Submitted" minlength="5" maxlength="5"
+           pattern="^[0,1]\d.\d\d"  required  data-error="You must enter a valid assignment number usually starting with 0.">
+    <input type="submit" value="Calculate Time" name = "submit">
+
+
 
 </form>
 </body>
+
 </html>
